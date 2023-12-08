ea4f322c92589b76ffba11d8743890e6867a7e13
Ilya Dragonromeo <you@example.com>
2023-12-08 17:35:59 +0300

    feat: add global & variables styles scss

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './assets/styles/global.scss';
 import AppRouter from './routes/router.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
