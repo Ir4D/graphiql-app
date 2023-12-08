d1f70f023cd2da1447858c15d969534f230a4193
Ir4D <iradedova@gmail.com>
2023-12-08 14:46:45 +0200

    feat: add styles for the Main Page

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import MainPage from './pages/MainPage/MainPage';
-import './index.css';
+import './assets/styles/global.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,22 +1,27 @@
 import styles from './MainPage.module.scss';
+import iconDocs from '../../assets/img/icons_docs3.png';
+import iconSettings from '../../assets/img/icons_settings3.png';
 
 const MainPage = () => {
   return (
     <main className={styles.main_page}>
-      <aside className={styles.main_menu}>
-        <p>Menu</p>
-        <button className={styles.menu_documentation}>Doc</button>
-        <button className={styles.menu_settings}>Set</button>
+      <aside className={styles.menu_wrapper}>
+        <button className={styles.menu_docs}>
+          <img src={iconDocs} alt="Docs" />
+        </button>
+        <button className={styles.menu_settings}>
+          <img src={iconSettings} alt="Settings" />
+        </button>
       </aside>
-      <section className={styles.main_content}>
-        <div className={styles.main_documentation}>
+      <section className={styles.content}>
+        <div className={styles.docs}>
           <p>Docs</p>
         </div>
-        <div className={styles.main_editor}>
-          <div className={styles.main_query}>
+        <div className={styles.editor_wrapper}>
+          <div className={styles.query}>
             <p>Query editor section</p>
           </div>
-          <div className={styles.main_viewer}>
+          <div className={styles.viewer}>
             <p>JSON viewer section</p>
           </div>
         </div>
