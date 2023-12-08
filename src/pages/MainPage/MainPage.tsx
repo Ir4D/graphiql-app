90f1fde62351e6b63d4f19765d8422a484e16765
Ir4D <iradedova@gmail.com>
2023-12-08 13:36:50 +0200

    feat: add Main Page layout

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,5 +1,28 @@
+import styles from './MainPage.module.scss';
+
 const MainPage = () => {
-  return <div>Main page</div>;
+  return (
+    <main className={styles.main_page}>
+      <aside className={styles.main_menu}>
+        <p>Menu</p>
+        <button className={styles.menu_documentation}>Doc</button>
+        <button className={styles.menu_settings}>Set</button>
+      </aside>
+      <section className={styles.main_content}>
+        <div className={styles.main_documentation}>
+          <p>Docs</p>
+        </div>
+        <div className={styles.main_editor}>
+          <div className={styles.main_query}>
+            <p>Query editor section</p>
+          </div>
+          <div className={styles.main_viewer}>
+            <p>JSON viewer section</p>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
 };
 
 export default MainPage;
