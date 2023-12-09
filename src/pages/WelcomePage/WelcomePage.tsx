88ef98a22f0250bcbae631b82e9c9c195f03b9df
Ir4D <iradedova@gmail.com>
2023-12-09 20:42:33 +0200

    feat: add Welcome page layout

--- src/pages/WelcomePage/WelcomePage.tsx
+++ src/pages/WelcomePage/WelcomePage.tsx
@@ -1,5 +1,24 @@
+import styles from './WelcomePage.module.scss';
+
 function WelcomePage() {
-  return <div>Hello Welcome Page!</div>;
+  return (
+    <div className={styles.welcome_page}>
+      <div className={styles.heading}>
+        <h1>Welcome to GraphiQL</h1>
+        <h3>GraphiQL is a playground/IDE for graphQL requests</h3>
+      </div>
+      <div className={styles.links}>
+        <div>
+          <p>Create your account of sign in here:</p>
+          <button>Sign in/ Sign up</button>
+        </div>
+        <div>
+          <p>Start making your requests now:</p>
+          <button>Let&apos;s go!</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default WelcomePage;
