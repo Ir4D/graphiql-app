70293255043c2e86650afba2974959ed48237b9d
Ilya Dragonromeo <you@example.com>
2023-12-10 19:53:22 +0300

    feat: remove header from outlet Layout

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,11 +1,9 @@
 import { Outlet } from 'react-router-dom';
 import { Footer } from './Footer/Footer';
-import { Header } from './Header/Header';
 
 export const Layout = () => {
   return (
     <div>
-      <Header />
       <Outlet />
       <Footer />
     </div>
