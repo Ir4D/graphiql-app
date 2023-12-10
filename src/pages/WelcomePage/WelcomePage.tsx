330b3a513f2e5477e28bcfcbbd33b20772b063e2
Ir4D <iradedova@gmail.com>
2023-12-10 11:06:05 +0200

    feat: add styles for welcome page

--- src/pages/WelcomePage/WelcomePage.tsx
+++ src/pages/WelcomePage/WelcomePage.tsx
@@ -3,18 +3,22 @@ import styles from './WelcomePage.module.scss';
 function WelcomePage() {
   return (
     <div className={styles.welcome_page}>
-      <div className={styles.heading}>
+      <div className={styles.welcome_heading}>
         <h1>Welcome to GraphiQL</h1>
         <h3>GraphiQL is a playground/IDE for graphQL requests</h3>
       </div>
-      <div className={styles.links}>
+      <div className={styles.links_wrapper}>
         <div>
-          <p>Create your account of sign in here:</p>
-          <button>Sign in/ Sign up</button>
+          <p>Create your account or sign in here:</p>
+          <button className={styles.btn}>
+            <a href="#">Sign in/ Sign up</a>
+          </button>
         </div>
         <div>
           <p>Start making your requests now:</p>
-          <button>Let&apos;s go!</button>
+          <button className={styles.btn}>
+            <a href="#">Let&apos;s go!</a>
+          </button>
         </div>
       </div>
     </div>
