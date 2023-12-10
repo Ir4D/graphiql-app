84496d8cecff4e9353895b01cfac5ab5c7955d65
Yuliya0503 <yuliyanarkevich0503@gmail.com>
2023-12-10 12:52:12 +0100

    refactor: change the authorization form using new components

--- src/components/AuthForm/AuthForm.tsx
+++ src/components/AuthForm/AuthForm.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import styles from './AuthForm.module.scss';
+import LoginForm from './LoginForm/LoginForm';
+import SignupForm from './SignupForm/SignupForm';
+import SlideControls from '../SlideControls/SlideControls';
 
 interface AuthFormProps {
   isLogin: boolean;
@@ -16,82 +19,16 @@ const AuthForm: React.FC<AuthFormProps> = ({
 }) => {
   return (
     <div className={styles.form_container}>
-      <div className={styles.slide_controls}>
-        <input
-          type="radio"
-          name="slider"
-          id="login"
-          checked={isLogin}
-          onChange={handleLoginClick}
-        />
-        <input
-          type="radio"
-          name="slider"
-          id="signup"
-          checked={!isLogin}
-          onChange={handleSignupClick}
-        />
-        <label
-          htmlFor="login"
-          className={`${styles.slide} ${
-            isLogin ? styles.login : styles.signup
-          }`}
-          onClick={handleLoginClick}
-        >
-          Login
-        </label>
-        <label
-          htmlFor="signup"
-          className={`${styles.slide} ${
-            isLogin ? styles.signup : styles.login
-          }`}
-          onClick={handleSignupClick}
-        >
-          Signup
-        </label>
-        <div
-          className={`${styles.slide_tab} ${
-            isLogin ? styles.tab_login : styles.tab_signup
-          }`}
-        ></div>
-      </div>
+      <SlideControls
+        isLogin={isLogin}
+        handleLoginClick={handleLoginClick}
+        handleSignupClick={handleSignupClick}
+      />
       <div className={styles.form_inner}>
         {isLogin ? (
-          <form className={`${styles.form} ${styles.login}`} action="#">
-            <div className={styles.field}>
-              <input type="text" placeholder="Email Address" />
-            </div>
-            <div className={styles.field}>
-              <input type="password" placeholder="Password" />
-            </div>
-            <div className={styles.pass_link}>
-              <a href="#">Forgot password?</a>
-            </div>
-            <div className={styles.field}>
-              <input type="submit" value="Login" />
-            </div>
-            <div className={styles.signup_link}>
-              Not a member?{' '}
-              <a href="#" onClick={handleSignupLinkClick}>
-                Signup now
-              </a>
-            </div>
-          </form>
+          <LoginForm onClick={handleSignupLinkClick} />
         ) : (
-          <form className={`${styles.form} ${styles.signup}`} action="#">
-            <div className={styles.field}>
-              <input type="text" placeholder="Email Address" />
-            </div>
-            <div className={styles.field}>
-              <input type="password" placeholder="Password" />
-            </div>
-            <div className={styles.field}>
-              <input type="password" placeholder="Confirm password" />
-            </div>
-            <div className={styles.field}>
-              <input type="submit" value="Signup" />
-            </div>
-          </form>
+          <SignupForm />
         )}
       </div>
     </div>
