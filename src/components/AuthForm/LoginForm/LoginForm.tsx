57c320c5195f96e29fef4c14382557de81d156aa
Yuliya0503 <yuliyanarkevich0503@gmail.com>
2023-12-10 12:48:12 +0100

    refactor: select separate components for the logging and registration form

---
+++ src/components/AuthForm/LoginForm/LoginForm.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import styles from '../AuthForm.module.scss';
+
+interface LoginFormProps {
+  onClick: (e: React.MouseEvent<HTMLAnchorElement>) => void;
+}
+
+const LoginForm: React.FC<LoginFormProps> = ({ onClick }) => {
+  return (
+    <form className={`${styles.form} ${styles.login}`} action="#">
+      <div className={styles.field}>
+        <input type="text" placeholder="Email Address" />
+      </div>
+      <div className={styles.field}>
+        <input type="password" placeholder="Password" />
+      </div>
+      <div className={styles.pass_link}>
+        <a href="#">Forgot password?</a>
+      </div>
+      <div className={styles.field}>
+        <input type="submit" value="Login" />
+      </div>
+      <div className={styles.signup_link}>
+        Not a member?{' '}
+        <a href="#" onClick={onClick}>
+          Signup now
+        </a>
+      </div>
+    </form>
+  );
+};
+
+export default LoginForm;
