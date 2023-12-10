74a9c5181244ad403fa43d1c4a1170c62740c308
Ilya Dragonromeo <you@example.com>
2023-12-10 15:14:41 +0300

    feat: add styles for language btns

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,7 +4,13 @@ export const Header = () => {
   return (
     <div className={styles.header}>
       <span>Welcome page link</span>
-      <span>Language switch</span>
+
+      <div className={styles.language_container}>
+        <button className={styles.language_item}>EN</button>
+        <span>/</span>
+        <button className={styles.language_item}>LP</button>
+      </div>
+
       <button className={styles.button}>Sign out</button>
     </div>
   );
