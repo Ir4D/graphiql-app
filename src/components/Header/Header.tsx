f7b7ebc3e0cb39f4785700c1cdb752bae85abc7f
Ilya Dragonromeo <you@example.com>
2023-12-10 16:44:07 +0300

    feat: add semantic

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,18 +3,22 @@ import styles from './Header.module.scss';
 
 export const Header = () => {
   return (
-    <div className={styles.header}>
-      <NavLink className={styles.link} to="/">
-        To welcome page
-      </NavLink>
+    <header className={styles.header}>
+      <div className={styles.container}>
+        <nav>
+          <NavLink className={styles.link} to="/">
+            To welcome page
+          </NavLink>
+        </nav>
 
-      <div className={styles.language_container}>
-        <button className={styles.language_item}>EN</button>
-        <span>/</span>
-        <button className={styles.language_item}>LP</button>
+        <div className={styles.language_container}>
+          <button className={styles.language_item}>EN</button>
+          <span>/</span>
+          <button className={styles.language_item}>LP</button>
+        </div>
+
+        <button className={styles.button}>Sign out</button>
       </div>
-
-      <button className={styles.button}>Sign out</button>
-    </div>
+    </header>
   );
 };
