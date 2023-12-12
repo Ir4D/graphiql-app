f07268a2aa876ea679b383ccef56f0fddbde4b17
Yuliya0503 <yuliyanarkevich0503@gmail.com>
2023-12-12 13:11:01 +0100

    feat: create validation schema for login

--- src/components/AuthForm/LoginForm/LoginForm.tsx
+++ src/components/AuthForm/LoginForm/LoginForm.tsx
@@ -1,18 +1,48 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
 import styles from '../AuthForm.module.scss';
+import { validationLoginSchema } from './validationLoginSchema';
 
 interface LoginFormProps {
   onClick: (e: React.MouseEvent<HTMLAnchorElement>) => void;
 }
 
 const LoginForm: React.FC<LoginFormProps> = ({ onClick }) => {
+  const { register, handleSubmit, formState } = useForm({
+    resolver: yupResolver(validationLoginSchema),
+  });
+
+  const { errors } = formState;
+
+  const onSubmit = (data: { email: string; password: string }) => {
+    /*создано исключительно для проверки*/
+    console.log(data);
+  };
+
   return (
-    <form className={`${styles.form} ${styles.login}`} action="#">
+    <form
+      className={`${styles.form} ${styles.login}`}
+      onSubmit={handleSubmit(onSubmit)}
+      action="#"
+    >
       <div className={styles.field}>
-        <input type="text" placeholder="Email Address" />
+        <input
+          type="text"
+          placeholder="Email Address"
+          {...register('email')}
+          required
+        />
+        <p>{errors.email?.message}</p>
       </div>
       <div className={styles.field}>
-        <input type="password" placeholder="Password" />
+        <input
+          type="password"
+          placeholder="Password"
+          {...register('password')}
+          required
+        />
+        <p>{errors.password?.message}</p>
       </div>
       <div className={styles.pass_link}>
         <a href="#">Forgot password?</a>
