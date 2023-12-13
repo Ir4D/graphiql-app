815a10f18ef698dd2facc2832bdb5c4dc347fa98
Ilya Dragonromeo <you@example.com>
2023-12-13 20:21:17 +0300

    fix: fix Irina last name in footer

--- src/components/Layout/Footer/Footer.tsx
+++ src/components/Layout/Footer/Footer.tsx
@@ -4,7 +4,7 @@ export const Footer = () => {
   return (
     <footer className={styles.footer}>
       <div className={styles.authors_container}>
-        <a href="https://github.com/Ir4D">Irina D.</a>
+        <a href="https://github.com/Ir4D">Irina Dedova</a>
         <a href="https://github.com/Yuliya0503">Yuliya_Narkevich</a>
         <a href="https://github.com/DragonRomeo">Ilya Romanov</a>
       </div>
