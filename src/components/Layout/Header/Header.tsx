3be9f71590447c10297941bfd4f4edfeba55bad1
Ilya Dragonromeo <you@example.com>
2023-12-13 19:21:24 +0300

    feat: add smooth header

--- src/components/Layout/Header/Header.tsx
+++ src/components/Layout/Header/Header.tsx
@@ -1,11 +1,42 @@
 import { NavLink } from 'react-router-dom';
 import styles from './Header.module.scss';
+import { useEffect, useState } from 'react';
 
 export const Header = () => {
   /*isLogin будет пропсой, которая приходит откуда-то выше */
   const isLogin = false;
+  const [isSticky, setIsSticky] = useState(false);
+
+  useEffect(() => {
+    let timeout;
+    let scroll = 0;
+
+    window.onscroll = () => {
+      if (timeout) {
+        clearTimeout(timeout);
+      }
+
+      timeout = setTimeout(() => {
+        /*scroll >= window.scrollY && */
+        if (window.scrollY > 10) {
+          console.log(`scroll =${scroll}, window.scroll=${window.scrollY}`);
+          setIsSticky(true);
+        } else {
+          setIsSticky(false);
+        }
+
+        scroll = window.scrollY;
+      }, 10);
+    };
+  }, []);
+  const headerClassName = isSticky
+    ? `${styles.header} ${styles.sticky}`
+    : styles.header;
+  console.log(headerClassName);
+  console.log(isSticky);
+
   return (
-    <header className={styles.header}>
+    <header className={headerClassName}>
       <div className={styles.container}>
         <nav>
           <NavLink className={styles.link} to="/">
