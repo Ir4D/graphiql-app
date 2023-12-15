1acd875fca5d95e9bd6c669817027956b4fcd387
Yuliya0503 <yuliyanarkevich0503@gmail.com>
2023-12-15 14:32:00 +0100

    feat: add localization to welcome page

--- src/pages/WelcomePage/WelcomePage.tsx
+++ src/pages/WelcomePage/WelcomePage.tsx
@@ -5,9 +5,10 @@ import avatarMale from '../../assets/img/avatar_male.png';
 import avatarFemale1 from '../../assets/img/avatar_female1.png';
 import avatarFemale2 from '../../assets/img/avatar_female2.png';
 import { Header } from '../../components/Layout/Header/Header';
-
+import { useLocalization } from '../../utils/localization/localizationContext';
 function WelcomePage() {
   const isAuthenticated = false;
+  const { locale, messages } = useLocalization();
 
   return (
     <>
@@ -15,24 +16,25 @@ function WelcomePage() {
       <div className={styles.welcome_page}>
         <section className={styles.welcome_wrapper}>
           <div className={styles.left_side}>
-            <h1 className={styles.heading}>Welcome to GraphiQL</h1>
+            <h1 className={styles.heading}>
+              {messages[locale].welcome_heading}
+            </h1>
             <div className={styles.description}>
-              <p>
-                GraphiQL is an in-browser IDE for writing, validating, and
-                testing GraphQL queries.
-              </p>
+              <p>{messages[locale].welcome_description}</p>
               <div className={styles.links_wrapper}>
                 {isAuthenticated ? (
                   <div>
                     <button className={styles.btn}>
-                      <Link to="/main">Let&apos;s start!</Link>
+                      <Link to="/main">{messages[locale].start_btn}</Link>
                     </button>
                   </div>
                 ) : (
                   <div>
-                    <p>Create your account or sign in here:</p>
+                    <p>{messages[locale].create_description}</p>
                     <button className={styles.btn}>
-                      <Link to="/auth">Sign in/ Sign up</Link>
+                      <Link to="/auth">
+                        {messages[locale].Sign_in} / {messages[locale].Sign_up}
+                      </Link>
                     </button>
                   </div>
                 )}
@@ -44,34 +46,30 @@ function WelcomePage() {
           </div>
         </section>
         <section className={styles.course_wrapper}>
-          <h3 className={styles.heading}>About the course</h3>
+          <h3 className={styles.heading}>{messages[locale].about_course}</h3>
           <div className={styles.text}>
             <p>
               <a className={styles.link} href="https://rs.school/react/">
-                RS School React Course
+                {messages[locale].RSS_course}
               </a>{' '}
-              is a crash course through the most important React topics, such as
-              React Router, redux, hooks, context and others.
+              {messages[locale].course_description_1}
             </p>
-            <p>
-              RS School is free-of-charge and community-based education program
-              conducted by The Rolling Scopes developer community since 2013.
-            </p>
+            <p>{messages[locale].course_description_2}</p>
           </div>
         </section>
         <section className={styles.team_wrapper}>
-          <h3 className={styles.heading}>Our team</h3>
+          <h3 className={styles.heading}>{messages[locale].team}</h3>
           <div className={styles.developers}>
             <div className={styles.developer}>
               <img src={avatarFemale1} alt="avatar" />
               <div className={styles.info}>
                 <p className={styles.name}>
                   <a href="https://github.com/Ir4D">
-                    Irina <br />
-                    Dedova
+                    {messages[locale].Irina} <br />
+                    {messages[locale].Dedova}
                   </a>
                 </p>
-                <p className={styles.role}>Team lead</p>
+                <p className={styles.role}>{messages[locale].team_lead}</p>
                 <p className={styles.contribution}>Welcome page, Main page</p>
               </div>
             </div>
@@ -80,11 +78,11 @@ function WelcomePage() {
               <div className={styles.info}>
                 <p className={styles.name}>
                   <a href="https://github.com/Yuliya0503">
-                    Yuliya <br />
-                    Narkevich
+                    {messages[locale].Yuliya} <br />
+                    {messages[locale].Narkevich}
                   </a>
                 </p>
-                <p className={styles.role}>Team member</p>
+                <p className={styles.role}>{messages[locale].team_member}</p>
                 <p className={styles.contribution}>
                   Sign in/ sign up page, routing, 404 page
                 </p>
@@ -95,11 +93,11 @@ function WelcomePage() {
               <div className={styles.info}>
                 <p className={styles.name}>
                   <a href="https://github.com/DragonRomeo">
-                    Ilya <br />
-                    Romanov
+                    {messages[locale].Ilya} <br />
+                    {messages[locale].Romanov}
                   </a>
                 </p>
-                <p className={styles.role}>Team member</p>
+                <p className={styles.role}>{messages[locale].team_member}</p>
                 <p className={styles.contribution}>
                   Header & footer, authentication
                 </p>
