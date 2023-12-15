807e04894096e4972f63763e7238c4e790b6ed5b
Yuliya0503 <yuliyanarkevich0503@gmail.com>
2023-12-15 14:39:29 +0100

    feat: add localization to main page

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -3,9 +3,11 @@ import styles from './MainPage.module.scss';
 import iconDocs from '../../assets/img/icons_docs.png';
 import iconSettings from '../../assets/img/icons_settings.png';
 import { Header } from '../../components/Layout/Header/Header';
+import { useLocalization } from '../../utils/localization/localizationContext';
 
 const MainPage = () => {
   const [docsPanelOpen, setDocsPanelOpen] = useState(false);
+  const { locale, messages } = useLocalization();
 
   const toggleDocsPanel = () => {
     setDocsPanelOpen((prevDocsPanelOpen) => !prevDocsPanelOpen);
@@ -27,7 +29,7 @@ const MainPage = () => {
           <div
             className={`${styles.docs} ${docsPanelOpen ? styles.active : ''}`}
           >
-            <h3 className={styles.docs_title}>Docs</h3>
+            <h3 className={styles.docs_title}>{messages[locale].docs_title}</h3>
             <p>Documentation...</p>
             <p>
               Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente
@@ -37,19 +39,23 @@ const MainPage = () => {
           <div className={styles.editor_wrapper}>
             <div className={styles.query}>
               <div className={styles.query_field}>
-                <h3 className={styles.query_title}>Query editor section</h3>
+                <h3 className={styles.query_title}>
+                  {messages[locale].query_title}
+                </h3>
               </div>
               <div className={styles.query_wrapper}>
                 <div className={styles.variables}>
-                  <h4>Variables</h4>
+                  <h4>{messages[locale].variables}</h4>
                 </div>
                 <div className={styles.headers}>
-                  <h4>Headers</h4>
+                  <h4>{messages[locale].headers}</h4>
                 </div>
               </div>
             </div>
             <div className={styles.viewer}>
-              <h3 className={styles.viewer_title}>JSON viewer section</h3>
+              <h3 className={styles.viewer_title}>
+                {messages[locale].viewer_title}
+              </h3>
             </div>
           </div>
         </section>
