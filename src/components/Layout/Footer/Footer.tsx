7052b7d3de10123b5f7890552e4ccab3b934a725
Yuliya0503 <yuliyanarkevich0503@gmail.com>
2023-12-15 11:24:24 +0100

    feat: add localization to footer

--- src/components/Layout/Footer/Footer.tsx
+++ src/components/Layout/Footer/Footer.tsx
@@ -1,12 +1,20 @@
 import styles from './Footer.module.scss';
+import { useLocalization } from '../../../utils/localization/localizationContext';
 
 export const Footer = () => {
+  const { locale, messages } = useLocalization();
   return (
     <footer className={styles.footer}>
       <div className={styles.authors_container}>
-        <a href="https://github.com/Ir4D">Irina_Dedova</a>
-        <a href="https://github.com/Yuliya0503">Yuliya_Narkevich</a>
-        <a href="https://github.com/DragonRomeo">Ilya_Romanov</a>
+        <a href="https://github.com/Ir4D">
+          {messages[locale].Irina}_{messages[locale].Dedova}
+        </a>
+        <a href="https://github.com/Yuliya0503">
+          {messages[locale].Yuliya}_{messages[locale].Narkevich}
+        </a>
+        <a href="https://github.com/DragonRomeo">
+          {messages[locale].Ilya}_{messages[locale].Romanov}
+        </a>
       </div>
 
       <div className={styles.data_container}>
