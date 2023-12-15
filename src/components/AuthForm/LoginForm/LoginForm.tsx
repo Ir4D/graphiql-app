75bcf61e11b0bf585dd85894d09ca66662e4517c
Yuliya0503 <yuliyanarkevich0503@gmail.com>
2023-12-15 12:01:10 +0100

    feat: add localization to login form

--- src/components/AuthForm/LoginForm/LoginForm.tsx
+++ src/components/AuthForm/LoginForm/LoginForm.tsx
@@ -5,12 +5,14 @@ import styles from '../AuthForm.module.scss';
 import { validationLoginSchema } from './validationLoginSchema';
 import { useNavigate } from 'react-router-dom';
 import { IFormLoginData } from '../../../models/forms';
+import { useLocalization } from '../../../utils/localization/localizationContext';
 
 interface LoginFormProps {
   onClick: (e: React.MouseEvent<HTMLAnchorElement>) => void;
 }
 
 const LoginForm: React.FC<LoginFormProps> = ({ onClick }) => {
+  const { locale, messages } = useLocalization();
   const navigate = useNavigate();
   const {
     register,
@@ -49,15 +51,19 @@ const LoginForm: React.FC<LoginFormProps> = ({ onClick }) => {
       onSubmit={handleSubmit(onSubmit)}
       action="#"
     >
-      {renderField('email', 'text', 'Email Address')}
-      {renderField('password', 'password', 'Password')}
+      {renderField('email', 'text', `${messages[locale].placeholder_email}`)}
+      {renderField(
+        'password',
+        'password',
+        `${messages[locale].placeholder_password}`
+      )}
       <div className={styles.pass_link}>
-        <a href="#">Forgot password?</a>
+        <a href="#">{messages[locale].pass_link}</a>
       </div>
       <div className={styles.field}>
         <input
           type="submit"
-          value="Sign In"
+          value={messages[locale].Sign_in}
           disabled={isDisabled}
           className={`${styles.submitButton} ${
             isDisabled ? styles.disabled : ''
@@ -65,9 +71,9 @@ const LoginForm: React.FC<LoginFormProps> = ({ onClick }) => {
         />
       </div>
       <div className={styles.signup_link}>
-        Not a member?{' '}
+        {messages[locale].not_a_member}{' '}
         <a href="#" onClick={onClick}>
-          Sign Up now
+          {messages[locale].Sign_Up_now}
         </a>
       </div>
     </form>
