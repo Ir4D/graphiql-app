9d3ff2298a2d360bb8f4689929493d870f8615eb
Yuliya0503 <yuliyanarkevich0503@gmail.com>
2023-12-15 10:36:29 +0100

    feat: add provider

--- src/utils/localization/localizationContext.tsx
+++ src/utils/localization/localizationContext.tsx
@@ -1,4 +1,6 @@
-import { createContext } from 'react';
+import React, { ReactNode, createContext, useState } from 'react';
+import en from './en.json';
+import ru from './ru.json';
 
 type Locale = 'en' | 'ru';
 type Messages = Record<string, string>;
@@ -9,8 +11,29 @@ interface LocalizationContextProps {
   changeLocale: (newLocale: Locale) => void;
 }
 
-export const LocalizationContext = createContext<LocalizationContextProps>({
+interface LocalizationProviderProps {
+  children: ReactNode;
+}
+
+const LocalizationContext = createContext<LocalizationContextProps>({
   locale: 'en',
   messages: { en: {}, ru: {} },
   changeLocale: () => {},
 });
+
+export const LocalizationProvider: React.FC<LocalizationProviderProps> = ({
+  children,
+}) => {
+  const [locale, setLocale] = useState<Locale>('en');
+  const messages: Record<Locale, Messages> = { en, ru };
+
+  const changeLocale = (newLocale: Locale) => {
+    setLocale(newLocale);
+  };
+
+  return (
+    <LocalizationContext.Provider value={{ locale, messages, changeLocale }}>
+      {children}
+    </LocalizationContext.Provider>
+  );
+};
