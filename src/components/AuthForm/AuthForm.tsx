c92a0be9322fb2e5b7a313c95eabc497410f3e44
Ilya Dragonromeo <you@example.com>
2023-12-17 19:05:00 +0300

    fix: comment extra code from authForm

--- src/components/AuthForm/AuthForm.tsx
+++ src/components/AuthForm/AuthForm.tsx
@@ -3,7 +3,7 @@ import styles from './AuthForm.module.scss';
 import LoginForm from './LoginForm/LoginForm';
 import SignupForm from './SignupForm/SignupForm';
 import SlideControls from '../SlideControls/SlideControls';
-import { useFirebaseContext } from '../../utils/firebase/FirebaseContext';
+// import { useFirebaseContext } from '../../utils/firebase/FirebaseContext';
 
 interface AuthFormProps {
   isLogin: boolean;
@@ -18,7 +18,7 @@ const AuthForm: React.FC<AuthFormProps> = ({
   handleSignupClick,
   handleSignupLinkClick,
 }) => {
-  const isLogin2 = ({ isLogin } = useFirebaseContext());
+  // const isLogin2 = ({ isLogin } = useFirebaseContext());
 
   return (
     <div className={styles.form_container}>
