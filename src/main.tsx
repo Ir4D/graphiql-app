3f328de3568b2852d8bdb0e1e8fef2ce9af9499c
Yuliya0503 <yuliyanarkevich0503@gmail.com>
2023-12-21 17:39:41 +0100

    fix: remove info

--- src/main.tsx
+++ src/main.tsx
@@ -4,15 +4,12 @@ import './assets/styles/global.scss';
 import AppRouter from './routes/router.tsx';
 import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary.tsx';
 import { LocalizationProvider } from './utils/localization/localizationContext.tsx';
-import { AuthProvider } from './utils/Auth/AuthContext.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ErrorBoundary>
       <LocalizationProvider>
-        <AuthProvider>
-          <AppRouter />
-        </AuthProvider>
+        <AppRouter />
       </LocalizationProvider>
     </ErrorBoundary>
   </React.StrictMode>
