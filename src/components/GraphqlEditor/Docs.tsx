9d5b3792121024ae1371bde639eaf41b173160c5
Ilya Dragonromeo <you@example.com>
2023-12-23 16:11:43 +0300

    feat: add Docs component

---
+++ src/components/GraphqlEditor/Docs.tsx
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react';
+import { useQueryContext } from '../../utils/QueryContext/QueryContext';
+import { getIntrospectionQuery, IntrospectionQuery } from 'graphql';
+
+export const Docs = () => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const { query, apiUrl } = useQueryContext();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(apiUrl, {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({ query: getIntrospectionQuery() }),
+        });
+
+        const result = await response.json();
+        setData(result.data);
+        setLoading(false);
+        console.log(result.data);
+      } catch (error) {
+        console.error('GraphQL request error:', error);
+      }
+    };
+
+    fetchData();
+  }, [apiUrl, query]);
+
+  return (
+    <div>
+      {loading ? (
+        <div>Loading...</div>
+      ) : (
+        <pre>{JSON.stringify(data, null, 2)}</pre>
+      )}
+    </div>
+  );
+};
