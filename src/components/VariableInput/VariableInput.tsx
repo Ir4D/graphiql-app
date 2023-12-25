8be24fe7fbc36daad955cba885b16e4ba4b9176b
Yuliya0503 <yuliyanarkevich0503@gmail.com>
2023-12-25 13:26:33 +0100

    feat: add localization

--- src/components/VariableInput/VariableInput.tsx
+++ src/components/VariableInput/VariableInput.tsx
@@ -26,18 +26,15 @@ const VariableInput: React.FC<VariableInputProps> = ({
 
   return (
     <div>
-      <div
-        onClick={handleToggle}
-        style={{ cursor: 'pointer', marginBottom: '10px' }}
-      >
-        <h4 className={styles.variable_item}>{messages[locale].variables}</h4>
-      </div>
+      <h4 className={styles.variable_item} onClick={handleToggle}>
+        {messages[locale].variables}
+      </h4>
       {isOpen && (
         <textarea
           className={styles.variable_text}
           value={variables}
           onChange={handleVariablesChange}
-          placeholder="Enter variables here"
+          placeholder={messages[locale].enter_variables_placeholder}
         />
       )}
     </div>
