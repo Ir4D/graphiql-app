062af9d3c448bd52aac22f3da986f5770fcbc2f9
Yuliya0503 <yuliyanarkevich0503@gmail.com>
2023-12-25 13:17:18 +0100

    refactor: use lint and prettier

--- src/components/AuthForm/Dashboard/Dashboard.tsx
+++ src/components/AuthForm/Dashboard/Dashboard.tsx
@@ -9,22 +9,22 @@ export const Dashboard = () => {
   const [user, loading] = useAuthState(auth);
   const [name, setName] = useState('');
   const navigate = useNavigate();
-  const fetchUserName = async () => {
-    try {
-      const q = query(collection(db, 'users'), where('uid', '==', user?.uid));
-      const doc = await getDocs(q);
-      const data = doc.docs[0].data();
-      setName(data.name);
-    } catch (err) {
-      console.error(err);
-      alert('An error occured while fetching user data');
-    }
-  };
   useEffect(() => {
+    const fetchUserName = async () => {
+      try {
+        const q = query(collection(db, 'users'), where('uid', '==', user?.uid));
+        const doc = await getDocs(q);
+        const data = doc.docs[0].data();
+        setName(data.name);
+      } catch (err) {
+        console.error(err);
+        alert('An error occured while fetching user data');
+      }
+    };
     if (loading) return;
     if (!user) return navigate('/');
     fetchUserName();
-  }, [user, loading]);
+  }, [user, loading, navigate]);
   return (
     <div className={styles.dashboard}>
       <div className={styles.dashboard__container}>
