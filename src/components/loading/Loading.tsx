3b5f613df649b77e5b45920ab8d2fc4473688334
Yuliya0503 <yuliyanarkevich0503@gmail.com>
2023-12-29 17:49:42 +0100

    feat: add img to loading page

--- src/components/loading/Loading.tsx
+++ src/components/loading/Loading.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import styles from './Loading.module.scss';
+import graphqlLogo from '../../assets/img/graphql_logo.png';
 
 const LoadingComponent: React.FC = () => {
   return (
-    <div className={styles.loading_wrapper}>
-      <h1>Loading...</h1>
+    <div className={styles.img_loading_conatiner}>
+      <img
+        className={styles.img_loading}
+        src={graphqlLogo}
+        alt="graphql logo"
+      />
     </div>
   );
 };
