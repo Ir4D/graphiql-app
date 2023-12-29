54bfb3eced345a27ffdbac6d9006768ca355fa9e
Yuliya0503 <yuliyanarkevich0503@gmail.com>
2023-12-29 16:51:52 +0100

    feat: optimize page loading on refresh

--- src/routes/PrivateRoute.tsx
+++ src/routes/PrivateRoute.tsx
@@ -1,14 +1,26 @@
-import React, { ReactNode } from 'react';
+import React, { ReactNode, useEffect, useState } from 'react';
 import { Navigate } from 'react-router-dom';
 import { auth } from '../utils/firebase/firebase';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import LoadingComponent from '../components/loading/Loading';
 
 interface PrivateRouteProps {
   children: ReactNode;
 }
 
 const PrivateRoute = ({ children }: PrivateRouteProps) => {
-  const [user] = useAuthState(auth);
+  const [user, loading] = useAuthState(auth);
+  const [authChecked, setAuthChecked] = useState(false);
+
+  useEffect(() => {
+    if (!loading) {
+      setAuthChecked(true);
+    }
+  }, [loading]);
+
+  if (!authChecked) {
+    return <LoadingComponent />;
+  }
 
   if (user) {
     return <>{children}</>;
--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode, Suspense, lazy } from 'react';
+import React, { ReactNode, Suspense, useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import WelcomePage from '../pages/WelcomePage/WelcomePage';
 import NotFoundPage from '../pages/NotFoundPage/NotFoundPage';
@@ -6,46 +6,64 @@ import { Layout } from '../components/Layout/Layout';
 import PrivateRoute from './PrivateRoute';
 import LoadingComponent from '../components/loading/Loading';
 
-const AuthPage = lazy(() => import('../pages/AuthPage/AuthPage'));
-const MainPage = lazy(() => import('../pages/MainPage/MainPage'));
+// Загружаем страницы асинхронно
+const AuthPage = React.lazy(() => import('../pages/AuthPage/AuthPage'));
+const MainPage = React.lazy(() => import('../pages/MainPage/MainPage'));
 
 interface AuthSuspenseProps {
   children: ReactNode;
 }
 
 const AuthSuspense = ({ children }: AuthSuspenseProps) => {
-  return <Suspense fallback={<LoadingComponent />}>{children}</Suspense>;
+  return <>{children}</>;
 };
 
 const AppRouter = () => {
+  const [isAuthLoaded, setAuthLoaded] = useState(false);
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      // Вместо этого нужно реализовать логику проверки аутентификации
+      setTimeout(() => {
+        setAuthLoaded(true);
+      }, 3500);
+    };
+
+    checkAuth();
+  }, []);
+
+  if (!isAuthLoaded) {
+    return <LoadingComponent />;
+  }
+
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route path="/" element={<WelcomePage />} />
-          <Route
-            path="/auth"
-            element={
-              <AuthSuspense>
-                <AuthPage />
-              </AuthSuspense>
-            }
-          />
-          <Route
-            path="/main"
-            element={
-              <AuthSuspense>
-                <PrivateRoute>
-                  <AuthSuspense>
+      <Suspense fallback={<LoadingComponent />}>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route path="/" element={<WelcomePage />} />
+            <Route
+              path="/auth"
+              element={
+                <AuthSuspense>
+                  <AuthPage />
+                </AuthSuspense>
+              }
+            />
+            <Route
+              path="/main"
+              element={
+                <AuthSuspense>
+                  <PrivateRoute>
                     <MainPage />
-                  </AuthSuspense>
-                </PrivateRoute>
-              </AuthSuspense>
-            }
-          />
-          <Route path="*" element={<NotFoundPage />} />
-        </Route>
-      </Routes>
+                  </PrivateRoute>
+                </AuthSuspense>
+              }
+            />
+            <Route path="*" element={<NotFoundPage />} />
+          </Route>
+        </Routes>
+      </Suspense>
     </Router>
   );
 };
