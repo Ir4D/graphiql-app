b3f886f158468a5927048e8e26d21dfd20cb16de
Yuliya0503 <yuliyanarkevich0503@gmail.com>
2023-12-30 12:14:38 +0100

    feat: add config to tests

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/tests/setupTests.ts',
+    css: true,
+    coverage: {
+      enabled: true,
+      all: true,
+      include: ['src/components/**/*', 'src/pages/**/*'],
+      exclude: ['**/*.d.ts'],
+      reporter: ['text'],
+      provider: 'v8',
+    },
+  },
 });
