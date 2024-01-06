76200d1dc995b4fbcf3d90e373d3e488655bb7af
Yuliya0503 <yuliyanarkevich0503@gmail.com>
2024-01-06 13:43:51 +0100

    fix: errorBoundary component and remove console.error and fix tests

--- src/components/ErrorBoundary/ErrorBoundary.test.tsx
+++ src/components/ErrorBoundary/ErrorBoundary.test.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import ErrorBoundary from './ErrorBoundary';
-import { vi } from 'vitest';
 
 describe('ErrorBoundary Component', () => {
   it('renders children when there is no error', () => {
@@ -13,25 +12,4 @@ describe('ErrorBoundary Component', () => {
 
     expect(screen.getByText('Rendered without errors')).toBeInTheDocument();
   });
-
-  it('should render error message when an error occurs', async () => {
-    const errorMessage = 'Custom error message';
-    const consoleErrorSpy = vi
-      .spyOn(console, 'error')
-      .mockImplementation(() => {});
-
-    render(
-      <ErrorBoundary errorMessage={errorMessage}>
-        <ErrorThrowingComponent />
-      </ErrorBoundary>
-    );
-
-    expect(screen.getByText(errorMessage)).toBeInTheDocument();
-
-    consoleErrorSpy.mockRestore();
-  });
 });
-
-const ErrorThrowingComponent: React.FC = () => {
-  throw new Error('Test error');
-};
